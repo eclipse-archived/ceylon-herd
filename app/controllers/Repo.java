3c1e642a78afae3e402a6ef74a72ad18572cc500
Guillaume LOURS <g.lours@agileit.fr>
2012-06-12 20:20:58 +0200

    Fix issue #56  - Add a menu entry  "My modules" and a list of the user's modules

--- app/controllers/Repo.java
+++ app/controllers/Repo.java
@@ -1,15 +1,9 @@
 package controllers;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-
 import models.Module;
 import models.ModuleVersion;
 import models.User;
-
 import org.apache.commons.lang.StringUtils;
-
 import play.Logger;
 import play.data.validation.Required;
 import play.data.validation.Validation;
@@ -17,6 +11,10 @@ import play.libs.MimeTypes;
 import play.mvc.Before;
 import util.JavaExtensions;
 import util.Util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class Repo extends MyController {
 
@@ -207,5 +205,13 @@ public class Repo extends MyController {
 		String filePath = file.getCanonicalPath();
 		if(!filePath.startsWith(repoPath))
 			forbidden("Path is not valid");
+	}
+
+	public static void myModules(){
+		User user = User.find("byUserName", Security.connected()).first();
+		List<models.Module> modules = models.Module.findByOwner(user);
+		boolean myModule =true;
+		flash.put("myModule", true);
+		index();
 	}
 }
