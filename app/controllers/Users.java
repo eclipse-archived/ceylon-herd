a8711a4e813c55c1ff4324c6b9cbca9035a53f6a
Guillaume LOURS <g.lours@agileit.fr>
2012-06-18 19:25:45 +0200

    Issue #32 - Edit User Profil
    use a secure controller and move form to the appropriate view.
    Stop using <code>validationFailed()<code> which display previous errors
    messages when calling <code>editForm</code> and <code>passwordForm</code

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -1,13 +1,8 @@
 package controllers;
 
 import models.Project;
-import play.data.validation.Email;
-import play.data.validation.MaxSize;
 import play.data.validation.Required;
-import play.data.validation.Validation;
-import play.libs.Codec;
 import play.mvc.Before;
-import util.Util;
 
 import java.util.List;
 
@@ -31,74 +26,4 @@ public class Users extends MyController {
 		render(user, ownedProjects);
 	}
 
-	public static void editForm(@Required String username){
-		models.User user = models.User.findRegisteredByUserName(username);
-		notFoundIfNull(user);
-
-		render(user);
-	}
-
-	public static void edit(@Required String username,
-							@MaxSize(Util.VARCHAR_SIZE) String firstName,
-							@MaxSize(Util.VARCHAR_SIZE) String lastName,
-							@MaxSize(Util.VARCHAR_SIZE) @Email String email){
-		models.User user = models.User.findByUserName(username);
-		notFoundIfNull(user);
-
-		if(validationFailed()){
-			editForm(username);
-		}
-
-		user.firstName = firstName;
-		user.lastName = lastName;
-		user.email = email;
-		user.save();
-
-		Users.view(username);
-
-	}
-
-	public static void passwordForm(@Required String username) {
-		models.User user = models.User.findRegisteredByUserName(username);
-		notFoundIfNull(user);
-
-		render(user);
-	}
-
-	public static void passwordEdit(@Required String username,
-									@Required @MaxSize(Util.VARCHAR_SIZE) String oldPassword,
-									@Required @MaxSize(Util.VARCHAR_SIZE) String password,
-									@Required @MaxSize(Util.VARCHAR_SIZE) String password2) {
-		models.User user = models.User.findRegisteredByUserName(username);
-		notFoundIfNull(user);
-
-		if(validationFailed()){
-			passwordForm(username);
-		}
-
-
-		if(!Codec.hexSHA1(user.salt + oldPassword).equals(user.password)){
-			Validation.addError("oldPassword","Wrong Password");
-			prepareForErrorRedirect();
-			passwordForm(username);
-		}
-
-		if(!password.equals(password2)) {
-			Validation.addError("password2","Confirmation password doesn't match the new password");
-			prepareForErrorRedirect();
-			passwordForm(username);
-		}
-
-		if(password.equals(oldPassword)){
-			Validation.addError("password", "Old and new password are the same!");
-			prepareForErrorRedirect();
-			passwordForm(username);
-		}
-
-		user.password = Codec.hexSHA1(user.salt + password);
-		user.save();
-
-
-		Users.view(username);
-	}
 }
