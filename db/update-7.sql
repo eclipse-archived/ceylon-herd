be318c31e71ca5e4b3b3a5dfa6692cea360607df
Stephane Epardaud <stef@epardaud.fr>
2012-06-22 17:24:59 +0200

    Updated db update file

--- db/update-7.sql
+++ db/update-7.sql
@@ -15,3 +15,26 @@ alter table moduleversion add column ceylonmajor int;
 update moduleversion set ceylonminor = 0, ceylonmajor = 0;
 alter table moduleversion ALTER COLUMN ceylonminor set not null;
 alter table moduleversion ALTER COLUMN ceylonmajor set not null;
+
+create table Author (
+    id int8 not null,
+    name TEXT,
+    primary key (id)
+);
+create table ModuleVersion_Author (
+    ModuleVersion_id int8 not null,
+    authors_id int8 not null
+);
+
+alter table ModuleVersion_Author 
+    add constraint FK9BE1449E7BF02576 
+    foreign key (ModuleVersion_id) 
+    references ModuleVersion;
+
+alter table ModuleVersion_Author 
+    add constraint FK9BE1449E42A7BA21 
+    foreign key (authors_id) 
+    references Author;
+
+alter table moduleversion add column doc text;
+alter table moduleversion add column license text;
