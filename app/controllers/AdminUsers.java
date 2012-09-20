a2dd1a149dbb6768c955afe7991bf06740d1a7b5
unknown <fmeurisse@HCB8HV1-S.office.masternaut.int>
2012-09-20 09:33:55 +0200

    User administration #91

---
+++ app/controllers/AdminUsers.java
@@ -0,0 +1,60 @@
+package controllers;
+
+import java.util.List;
+
+import play.data.validation.Email;
+import play.data.validation.MaxSize;
+import play.data.validation.Required;
+import play.data.validation.URL;
+import util.Util;
+
+import models.User;
+
+public class AdminUsers extends LoggedInController {
+
+    @Check("admin")
+	public static void index(int page, int pageSize) {
+    	if (page < 1) {
+    		page = 1;
+    	}
+    	if (pageSize < 1) {
+    		pageSize = 20;
+    	}
+    	List<User> users = User.all().fetch(page, pageSize);
+    	long userCount = User.count();
+		int pageCount = (int) Math.ceil((float) userCount / pageSize);
+		render(users, page, pageCount, pageSize);
+	}
+
+    @Check("admin")
+    public static void editForm(Long id) {
+    	notFoundIfNull(id);
+    	User editedUser = User.findById(id);
+    	notFoundIfNull(editedUser);
+    	render(editedUser);
+    }
+
+    @Check("admin")
+    public static void edit(@Required Long id, 
+	        @Required @MaxSize(Util.VARCHAR_SIZE) @Email String email, 
+	        @MaxSize(Util.VARCHAR_SIZE) String firstName, 
+	        @MaxSize(Util.VARCHAR_SIZE) String lastName, 
+	        @Required Boolean isAdmin) {
+    	if(validationFailed()) {
+    		editForm(id);
+    	}
+    	
+    	User editedUser = User.findById(id);
+    	notFoundIfNull(editedUser);
+    	editedUser.email = email;
+    	editedUser.firstName = firstName;
+    	editedUser.lastName = lastName;
+    	editedUser.isAdmin = isAdmin;
+    	editedUser.save();
+    	
+    	flash("message", "The user has been modified.");
+    	
+    	index(1, 20);
+    }
+    
+}
