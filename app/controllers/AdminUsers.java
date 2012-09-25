bfb8f13949f7d54015bd38ed75bbd36d4a2cb1b3
Stephane Epardaud <stef@epardaud.fr>
2012-09-25 11:31:59 +0200

    LoggedInUsers: moved user edit form from admin to user #32

--- app/controllers/AdminUsers.java
+++ app/controllers/AdminUsers.java
@@ -3,10 +3,6 @@ package controllers;
 import java.util.List;
 
 import models.User;
-import play.data.validation.Email;
-import play.data.validation.MaxSize;
-import play.data.validation.Required;
-import util.Util;
 
 @Check("admin")
 public class AdminUsers extends LoggedInController {
@@ -23,35 +19,5 @@ public class AdminUsers extends LoggedInController {
 		int pageCount = (int) Math.ceil((float) userCount / pageSize);
 		render(users, page, pageCount, pageSize);
 	}
-
-    public static void editForm(Long id) {
-    	notFoundIfNull(id);
-    	User editedUser = User.findById(id);
-    	notFoundIfNull(editedUser);
-    	render(editedUser);
-    }
-
-    public static void edit(@Required Long id, 
-	        @Required @MaxSize(Util.VARCHAR_SIZE) @Email String email, 
-	        @MaxSize(Util.VARCHAR_SIZE) String firstName, 
-	        @MaxSize(Util.VARCHAR_SIZE) String lastName, 
-	        boolean isAdmin) {
-    	if(validationFailed()) {
-    		editForm(id);
-    	}
-    	
-    	User editedUser = User.findById(id);
-    	notFoundIfNull(editedUser);
-    	
-    	editedUser.email = email;
-    	editedUser.firstName = firstName;
-    	editedUser.lastName = lastName;
-    	editedUser.isAdmin = isAdmin;
-    	editedUser.save();
-    	
-    	flash("message", "The user has been modified.");
-    	
-    	index(1, 20);
-    }
     
 }
