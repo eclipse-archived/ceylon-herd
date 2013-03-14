6db2a5fac4bd4099abbab0410415ee1c453462d7
thradec <thradec@gmail.com>
2013-03-14 14:45:56 +0100

    Issue #109: support for syntax highlighting

---
+++ public/javascripts/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    initSyntaxHighlighter();
+    automaticSyntaxHighlighingPreTags();
+});
+
+function initSyntaxHighlighter() {
+    try {
+        if (SyntaxHighlighter != null) {
+            SyntaxHighlighter.defaults['gutter'] = false;
+            SyntaxHighlighter.defaults['toolbar'] = false;
+            SyntaxHighlighter.all();
+        }
+    } catch (error) {
+        if (error instanceof ReferenceError || error instanceof TypeError) {
+            // ignore this one
+        } else {
+            throw error;
+        }
+    }
+}
+
+function automaticSyntaxHighlighingPreTags() {
+    $("pre").each(function() {
+        var pre = $(this);
+        if (!pre.hasClass("brush:")) {
+            pre.addClass("brush: ceylon");
+            pre.hide();
+        }
+    });
+}
