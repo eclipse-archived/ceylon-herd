87bafcb420403f75ad8b0e9beabb6f9113b9c403
thradec <thradec@gmail.com>
2013-03-19 11:50:50 +0100

    Issue #109: support for markdown links to declarations

--- app/util/JavaExtensions.java
+++ app/util/JavaExtensions.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
@@ -14,11 +16,16 @@ import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 
+import org.apache.commons.lang.StringUtils;
+
+import models.ModuleVersion;
 import models.Upload;
 import play.Logger;
+import play.exceptions.NoRouteFoundException;
 import play.i18n.Lang;
 import play.mvc.Http.Cookie;
 import play.mvc.Http.Request;
+import play.mvc.Router;
 import play.mvc.Router.ActionDefinition;
 import play.templates.BaseTemplate;
 import play.templates.BaseTemplate.RawData;
@@ -100,12 +107,16 @@ public class JavaExtensions extends play.templates.JavaExtensions {
 	}
 	
 	public static Object md(String mdString) {
+	    return md(mdString, null);
+	}
+	
+	public static Object md(String mdString, ModuleVersion module) {
 	    String escaped = HTML.htmlEscape(mdString);
 	    
 	    Configuration config = Configuration.builder()
 	            .forceExtentedProfile()
 	            .setCodeBlockEmitter(MarkdownBlockEmitter.INSTANCE)
-	            .setSpecialLinkEmitter(MarkdownSpanEmitter.INSTANCE)
+	            .setSpecialLinkEmitter(module == null ? MarkdownSpanEmitter.INSTANCE : new MarkdownSpanEmitter(module))
 	            .build();
 	    String html = Processor.process(escaped, config);        
 
@@ -315,12 +326,188 @@ public class JavaExtensions extends play.templates.JavaExtensions {
     
     private static class MarkdownSpanEmitter implements SpanEmitter {
 
-        private static MarkdownSpanEmitter INSTANCE = new MarkdownSpanEmitter();
+        private static final MarkdownSpanEmitter INSTANCE = new MarkdownSpanEmitter(null);
+        
+        private static final String DOT_SEPARATOR = ".";
+        private static final String PIPE_SEPARATOR = "|";
+        private static final String PATH_SEPARATOR = "/";
+        private static final String PACKAGE_SEPARATOR = "::";
+        private static final String MODULE_DOC = "module-doc";
+        
+        private final ModuleVersion currentModule;
+        
+        public MarkdownSpanEmitter(ModuleVersion currentModule) {
+            this.currentModule = currentModule;
+        }
 
         @Override
         public void emitSpan(StringBuilder out, String content) {
-            // TODO process markdown links
-            out.append("[").append(content).append("]");
+            if (currentModule == null || StringUtils.isEmpty(content)) {
+                printUnresolvableLink(out, content);
+                return;
+            }        
+            
+            String namePart = resolveNamePart(content);
+            String declPart = resolveDeclPart(content);
+            String declName = resolveDeclName(declPart);
+            String packageName = resolvePackageName(declPart);
+            
+            ModuleVersion module = resolveModule(packageName);
+            String moduleDocUrl = resolveModuleDocUrl(module);
+            if (moduleDocUrl == null) {
+                printUnresolvableLink(out, content);
+                return;
+            }
+            
+            String packagePath = resolvePackagePath(module, packageName);
+            
+            String[] tuple = resolveFileNameAndAnchor(declName);
+            String fileName = tuple[0];
+            String anchor = tuple[1];
+            
+            if( docFileExists(module, packagePath, fileName) ) {
+                printLink(out, namePart, moduleDocUrl, packagePath, fileName, anchor);
+            }
+            else {
+                printUnresolvableLink(out, content);
+            }
+        }
+
+        private void printUnresolvableLink(StringBuilder out, String content) {
+            out.append("[[").append(content).append("]]");
+        }
+
+        private void printLink(StringBuilder out, String namePart, String moduleDocUrl, String packagePath, String fileName, String anchor) {
+            out.append("<a href='");
+            out.append(moduleDocUrl);
+            if (!packagePath.isEmpty()) {
+                out.append(packagePath);
+                out.append(PATH_SEPARATOR);
+            }
+            out.append(fileName);
+            if (anchor != null) {
+                out.append("#");
+                out.append(anchor);
+            }
+            out.append("'>");
+            out.append(namePart);
+            out.append("</a>");
+        }
+
+        private String resolveNamePart(String content) {
+            int pipeSeparatorIndex = content.indexOf(PIPE_SEPARATOR);
+            if (pipeSeparatorIndex != -1) {
+                return content.substring(0, pipeSeparatorIndex);
+            } else {
+                return content;
+            }
+        }
+
+        private String resolveDeclPart(String content) {
+            int pipeSeparatorIndex = content.indexOf(PIPE_SEPARATOR);
+            if (pipeSeparatorIndex != -1) {
+                return content.substring(pipeSeparatorIndex + 1, content.length());
+            } else {
+                return content;
+            }
+        }
+
+        private String resolveDeclName(String declPart) {
+            int packageSeparatorIndex = declPart.indexOf(PACKAGE_SEPARATOR);
+            if (packageSeparatorIndex != -1) {
+                return declPart.substring(packageSeparatorIndex + 2, declPart.length());
+            } else {
+                return declPart;
+            }
+        }
+
+        private String resolvePackageName(String declPart) {
+            int packageSeparatorIndex = declPart.indexOf(PACKAGE_SEPARATOR);
+            if (packageSeparatorIndex != -1) {
+                return declPart.substring(0, packageSeparatorIndex);
+            } else {
+                return currentModule.module.name; // default package is module root
+            }
+        }
+
+        private String resolvePackagePath(ModuleVersion moduleVersion, String packageName) {
+            String packagePath = packageName.substring(moduleVersion.module.name.length());
+            if (packagePath.startsWith(DOT_SEPARATOR)) {
+                packagePath = packagePath.substring(1);
+            }
+            return packagePath;
+        }
+
+        private ModuleVersion resolveModule(String packageName) {
+            if (packageName.startsWith(currentModule.module.name)) {
+                return currentModule;
+            } else {
+                List<ModuleVersion> dependencies = currentModule.getDependentModuleVersions(); // TODO optimize
+                for (ModuleVersion dependency : dependencies) {
+                    if (packageName.startsWith(dependency.module.name)) {
+                        return dependency;
+                    }
+                }
+                return null;
+            }
+        }        
+
+        private String resolveModuleDocUrl(ModuleVersion moduleVersion) {
+            if( moduleVersion != null ) {
+                StringBuilder path = new StringBuilder();
+                path.append(moduleVersion.module.name.replace(DOT_SEPARATOR, PATH_SEPARATOR));
+                path.append(PATH_SEPARATOR);
+                path.append(moduleVersion.version);
+                path.append(PATH_SEPARATOR);
+                path.append(MODULE_DOC);
+                path.append(PATH_SEPARATOR);
+
+                try {
+                    return Router.reverse("Repo.viewFile", Collections.singletonMap("path", (Object) path.toString())).url;
+                } catch (NoRouteFoundException e) {
+                    // noop
+                }
+            }
+            return null;
+        }
+
+        private String[] resolveFileNameAndAnchor(String declName) {
+            String fileName = null;
+            String anchor = null;
+        
+            String[] names = declName.split("\\" + DOT_SEPARATOR);
+            String lastName = names[names.length - 1];
+            String prevLastName = names.length > 1 ? names[names.length - 2] : null;
+            String rest = names.length > 1 ? declName.substring(0, declName.lastIndexOf(DOT_SEPARATOR)) + "." : "";
+        
+            if (Character.isUpperCase(lastName.charAt(0))) {
+                fileName = rest + lastName + ".type.html";
+            }
+            else if (prevLastName != null && Character.isUpperCase(prevLastName.charAt(0))) {
+                fileName = rest + "type.html";
+                anchor = lastName;
+            }
+            else if (prevLastName != null && Character.isLowerCase(prevLastName.charAt(0))) {
+                fileName = rest + "object.html";
+                anchor = lastName;
+            }
+            else {
+                fileName = "index.html";
+                anchor = lastName;
+            }
+        
+            return new String[] { fileName, anchor };
+        }
+
+        private boolean docFileExists(ModuleVersion moduleVersion, String packagePath, String fileName) {
+            File repoDir = Util.getRepoDir();
+            File moduleDir = new File(repoDir, moduleVersion.module.name.replace(DOT_SEPARATOR, PATH_SEPARATOR) + PATH_SEPARATOR + moduleVersion.version + PATH_SEPARATOR + MODULE_DOC);
+            File packageDir = new File(moduleDir, packagePath);
+            File f = new File(packageDir, fileName);
+            if (f.exists() && f.isFile()) {
+                return true;
+            }
+            return false;
         }
 
     }
