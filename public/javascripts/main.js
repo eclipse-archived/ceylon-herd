771e60f6dfda43d41e547e5926e8160d4d44b3ff
thradec <thradec@gmail.com>
2013-04-18 09:18:13 +0200

    Add key bindings for actions #42

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     initSyntaxHighlighter();
     automaticSyntaxHighlighingPreTags();
     initStarRating();
+    initSelectableTable();
+    initSearchAndInfoShortcut();
 });
 
 function initSyntaxHighlighter() {
@@ -49,4 +51,111 @@ function initStarRating() {
         starRating.find(".star").removeClass("star-full");
         starRating.find("input[name='rating']").val("0");
     });
+}
+
+function initSelectableTable() {
+    if( $(".table-selectable").length != 1 ) {
+        return;
+    }
+    
+    initMouseMoveHandler();
+    initKeyboardShortcutsHandler();
+    initKeyboardShortcutsInfo();
+
+    function initMouseMoveHandler() {
+        $(".table-selectable tr").mousemove(function () {
+            selectRow($(this));
+        });
+    }
+    
+    function initKeyboardShortcutsHandler() {
+        $(document).keypress(function(e) {
+            if (isEventTargetInput(e)) {
+                return;
+            }
+            if (e.charCode == 106 /* j */) {
+                moveSelectionDown();
+            }
+            else if (e.charCode == 107 /* k */) {
+                moveSelectionUp();
+            }
+            else if (e.keyCode == 13 /* enter */) {
+                openSelectedRow();
+            }
+        });
+    }
+    
+    function initKeyboardShortcutsInfo() {
+        var p = $("#keyboard-shortcuts-panel");
+        var k = $(".key-j, .key-k, .key-enter", p);
+        k.find(".badge").addClass("badge-info");
+        k.find(".muted").removeClass("muted").addClass("text-info");
+    }
+    
+    function moveSelectionDown() {
+        var trSelected = getSelectedRow();
+        if( trSelected.length == 1 ) {
+            selectRow(trSelected.next());
+        }
+        else {
+            selectRow($(".table-selectable tbody tr:first"));
+        }
+    }
+    
+    function moveSelectionUp() {
+        var trSelected = getSelectedRow();
+        selectRow(trSelected.prev());
+    }
+    
+    function openSelectedRow() {
+        var trSelected = getSelectedRow();
+        if (trSelected.length == 1) {
+            trHref = $("a:first", trSelected).attr("href");
+            if (trHref) {
+                document.location = trHref;
+            }
+        }
+    }
+    
+    function getSelectedRow() {
+        return $(".table-selectable tbody tr.tr-selected");
+    }
+    
+    function selectRow(row) {
+        if (row.is("tr") && !row.hasClass("tr-selected")) {
+            $(".table-selectable tbody tr.tr-selected").removeClass("tr-selected");
+            row.addClass("tr-selected");
+        }
+    }  
+}
+
+function initSearchAndInfoShortcut() {
+    var searchInput = $("input[name='q']")
+    if (searchInput.length == 1) {
+        $(document).keypress(function(e) {
+            if (isEventTargetInput(e)) {
+                return;
+            }
+            if (e.charCode == 115 /* s - focus search */) {
+                e.preventDefault();
+                searchInput.focus();
+            }
+            if (e.charCode == 63 /* ? - open info */) {
+                $('#keyboard-shortcuts-dropdown > .dropdown-toggle').click();
+            }
+        });
+        searchInput.keydown(function(e) {
+            if (e.keyCode == 27 /* esc - unfocus seach */) {
+                searchInput.blur();
+            }
+        });
+    }
+}
+
+function isEventTargetInput(e) {
+    var targetName = e.target.tagName.toLowerCase();
+    if (targetName == "input" || targetName == "textarea") {
+        return true;
+    }
+    return false;
 }
